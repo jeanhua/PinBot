81db50e08d9b4246624f03f9522be890001558f4
jeanhua <2207739460@qq.com>
2025-08-01 19:37:41 +0800

    修改日志插件

--- plugins/examplePlugin.go
+++ plugins/examplePlugin.go
@@ -11,11 +11,11 @@ import (
 var ExamplePlugin = botcontext.NewPluginContext("example plugin", examplePluginOnFriend, examplePluginOnGroup, "示例插件")
 
 func examplePluginOnFriend(message *model.FriendMessage) bool {
-	log.Printf("[私聊消息](%s):%s\n", message.Sender.Nickname, utils.ExtractPrivateMessageText(message))
+	log.Printf("[私聊消息](%s %d):%s\n", message.Sender.Nickname, message.Sender.UserId, utils.ExtractPrivateMessageText(message))
 	return true
 }
 func examplePluginOnGroup(message *model.GroupMessage) bool {
 	text, _ := utils.ExtractMessageContent(message)
-	log.Printf("[群聊消息](%s):%s\n", message.Sender.Nickname, text)
+	log.Printf("[群聊消息(%d)](%s):%s\n", message.GroupId, message.Sender.Nickname, text)
 	return true
 }
